fd2ed0a347c8b23846c12b262cbfae176b825d7b
Mateoo18 <mateusz.nowak.203@gmail.com>
2023-12-05 12:17:24 +0100

    Commit with full solution

--- src/main/java/org/simplestore/Main.java
+++ src/main/java/org/simplestore/Main.java
@@ -1,20 +1,36 @@
 package org.simplestore;
 
 import org.simplestore.model.Inventory;
+import org.simplestore.model.Product;
+import org.simplestore.model.ProductNotFoundException;
+import org.simplestore.service.ShoppingCart;
 import org.simplestore.util.InventoryLoader;
 
+import java.util.ArrayList;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ProductNotFoundException {
         Inventory inventory = new Inventory();
-        InventoryLoader.loadInventory("path/to/inventory.txt", inventory);  // TODO: Change to proper path (example file is in project resources)
+        InventoryLoader.loadInventory("/home/mateusz/Pulpit/Java/ShopSim/src/main/resources/inventory.txt", inventory);
+        ShoppingCart shoppingCart=new ShoppingCart(inventory);
+        shoppingCart.addItem(1,1);
+        shoppingCart.addItem(10,2);
+        System.out.println("Total cost: "+shoppingCart.calculateTotalCost());
+        shoppingCart.clearCart();
 
-        // Implement example usage of application components
+        Product product1=new Product(11,"Plastic car",20.00);
+        inventory.addProduct(product1);
+        ArrayList<Product> products = (ArrayList<Product>) inventory.listAllProducts();
 
-        // TODO: Create shopping carts, add products,
-        //  clear cart, handle exceptions, etc.
+        for(int i=0;i<products.size();i++){
+            System.out.println(products.get(i).toString());
+        }
+        inventory.removeproduct(4);
+        ArrayList<Product> products1 = (ArrayList<Product>) inventory.listAllProducts();
 
-        // TODO: Add product to inventory, list all products
-        // TODO: Remove product from inventory, list all products
+        for ( Product product:products1){
+            System.out.println(product.toString());
+        }
     }
 }
 
--- src/test/java/org/simplestore/service/ShoppingCartConcurrencyTest.java
+++ src/test/java/org/simplestore/service/ShoppingCartConcurrencyTest.java
@@ -7,25 +7,58 @@ import org.simplestore.model.ProductNotFoundException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 public class ShoppingCartConcurrencyTest {
     private final Inventory inventory = new Inventory();
 
     @Test
-    void addAndRemoveItemsConcurrently() throws InterruptedException {
+    void addAndRemoveItemsConcurrently() throws InterruptedException{
         ShoppingCart shoppingCart = new ShoppingCart(inventory);
         inventory.addProduct(new Product(1, "Test Product", 10.0));
+        int totalThreads = 3;
+        AtomicInteger itemsToAdd = new AtomicInteger(14);
+        AtomicInteger itemsToRemove = new AtomicInteger(3);
 
-        // Prepare tests with 10 threads. Next:
+        List<Thread> threads = new ArrayList<>();
 
-        // TODO Add 100 items concurrently
+        for (int i = 0; i < totalThreads; i++) {
+            threads.add(new Thread(() -> {
+                while (itemsToAdd.getAndDecrement() > 0) {
+                    shoppingCart.addItem(1, 1);
+                }
+            }));
+        }
 
-        // TODO Remove 50 items concurrently
+        for (Thread thread : threads) {
+            thread.start();
+        }
 
-        // TODO Await for threads termination, eg. join
+        for (Thread thread : threads) {
+            thread.join();
+        }
 
-        // Check if the final quantity is as expected
-        assertEquals(50, shoppingCart.getItemQuantity(1));
+        threads.clear();
+
+        for (int i = 0; i < totalThreads; i++) {
+            threads.add(new Thread(() -> {
+                while (itemsToRemove.getAndDecrement() > 0){
+                    shoppingCart.removeItem(1, 1);
+                }
+            }));
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        assertEquals(11, shoppingCart.getItemQuantity(1));
     }
 
     @Test
@@ -33,12 +66,20 @@ public class ShoppingCartConcurrencyTest {
         ShoppingCart shoppingCart = new ShoppingCart(inventory);
         inventory.addProduct(new Product(1, "Test Product", 10.0));
 
-        // TODO Add 100 items concurrently
-        // TODO Await for threads termination, eg. join
+        int totalThreads = 10;
+        int itemsToAdd = 100;
 
-        // Check if the total cost calculation is correct
+        ExecutorService executor = Executors.newFixedThreadPool(totalThreads);
+        for (int i = 0; i < itemsToAdd; i++) {
+            executor.execute(() -> {
+                    shoppingCart.addItem(1, 1);
+            });
+        }
+
+        executor.shutdown();
+        executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        //Uzywanie executor jest lepsze pod wzgledem tworzenai wielu watkow
         assertEquals(1000.0, shoppingCart.calculateTotalCost());
     }
 
-    // Note for presenter: Discuss the importance of concurrency testing in a multi-threaded environment.
 }
