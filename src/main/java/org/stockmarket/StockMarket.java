85a721ddec86f8aaddad5b7b19b38bb026000525
Mateoo18 <mateusz.nowak.203@gmail.com>
2024-01-17 08:49:33 +0100

    commit with full solution

--- src/main/java/org/stockmarket/StockMarket.java
+++ src/main/java/org/stockmarket/StockMarket.java
@@ -19,10 +19,6 @@ public class StockMarket {
         stocks.add(stock);
     }
 
-    // TODO zaimplementuj metodę, która zwraca opisy wszystkich akcje danego typu, razem z opisem, np.
-    //  "AAPL - Technology and innovation sector - $150.0",
-    //  "MSFT - Technology and innovation sector - $200.0",
-    //  ...
     public List<String> listStocksByType(StockType type) {
         return stocks.stream()
                 .filter(stock -> stock.getType() == type)
@@ -30,7 +26,6 @@ public class StockMarket {
                 .collect(Collectors.toList());
     }
 
-    // TODO zaimplementuj metodę, która ustawia nową cenę danej akcji
     public void updateStockPrice(String symbol, double newPrice) {
         stocks.stream()
                 .filter(stock -> stock.getSymbol().equals(symbol))
@@ -38,15 +33,12 @@ public class StockMarket {
                 .ifPresent(stock -> stock.setPrice(newPrice));
     }
 
-    // TODO zaimplentuj metodę, która zwraca daną akcję.
-    //  Użyj optional na wypadek, gdyby nie było akcji o danym symbolu.
     public Optional<Stock> getStock(String symbol) {
         return stocks.stream()
                 .filter(stock -> stock.getSymbol().equals(symbol))
                 .findFirst();
     }
 
-    // TODO zaimplentuj metodę, która zwraca wszystkie akcje
     public List<Stock> getStocks() {
         return new ArrayList<>(stocks);
     }
