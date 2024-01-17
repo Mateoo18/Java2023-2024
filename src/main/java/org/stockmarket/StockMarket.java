e3214cbd72cfc16ac041ed0ea95ce502310ab305
Mateoo18 <mateusz.nowak.203@gmail.com>
2024-01-17 08:33:45 +0100

    commit with solution

--- src/main/java/org/stockmarket/StockMarket.java
+++ src/main/java/org/stockmarket/StockMarket.java
@@ -24,22 +24,30 @@ public class StockMarket {
     //  "MSFT - Technology and innovation sector - $200.0",
     //  ...
     public List<String> listStocksByType(StockType type) {
-        // TODO
+        return stocks.stream()
+                .filter(stock -> stock.getType() == type)
+                .map(stock -> String.format("%s - %s - $%.2f", stock.getSymbol(), stock.getTypeDescription(), stock.getPrice()))
+                .collect(Collectors.toList());
     }
 
     // TODO zaimplementuj metodę, która ustawia nową cenę danej akcji
     public void updateStockPrice(String symbol, double newPrice) {
-        // TODO
+        stocks.stream()
+                .filter(stock -> stock.getSymbol().equals(symbol))
+                .findFirst()
+                .ifPresent(stock -> stock.setPrice(newPrice));
     }
 
     // TODO zaimplentuj metodę, która zwraca daną akcję.
     //  Użyj optional na wypadek, gdyby nie było akcji o danym symbolu.
     public Optional<Stock> getStock(String symbol) {
-        // TODO
+        return stocks.stream()
+                .filter(stock -> stock.getSymbol().equals(symbol))
+                .findFirst();
     }
 
     // TODO zaimplentuj metodę, która zwraca wszystkie akcje
     public List<Stock> getStocks() {
-        // TODO
+        return new ArrayList<>(stocks);
     }
 }
--- src/main/java/org/stockmarket/model/StockType.java
+++ src/main/java/org/stockmarket/model/StockType.java
@@ -10,5 +10,16 @@ public enum StockType {
     private final String name;
     private final String description;
 
-    // TODO dokończ implementację enum, uzupełniając go o konstruktor oraz gettery
+    StockType(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
